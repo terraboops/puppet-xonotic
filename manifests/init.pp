45236fa1b2e987220949a6efd78ab7d3eadc8916
Tyler Mauthe <tmauthe@hatsize.com>
2015-12-07 01:48:37 -0700

    Add basic apt install

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,5 +11,19 @@
 # Copyright 2015 Tyler Mauthe, unless otherwise noted.
 #
 class xonotic {
-
+	require ::apt
+	# Xonotic - playdeb repo
+	apt::source { 'playdeb':
+		location => 'http://archive.getdeb.net/ubuntu',
+		release  => 'wily-getdeb',
+		repos    => 'games',
+		key      => {
+			id     => '1958A549614CE21CFC27F4BAA8A515F046D7E7CF',
+			source => 'http://archive.getdeb.net/getdeb-archive.key',
+		}
+	}
+	package { 'xonotic':
+		ensure  => 'latest',
+		require => Apt::Source['playdeb']
+	}
 }
