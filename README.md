b410bca61f2e4c5616d9503572b466db69758e1f
Tyler Mauthe <tmauthe@hatsize.com>
2015-12-06 19:52:38 -0700

    Initial skeleton

---
+++ README.md
@@ -0,0 +1,5 @@
+# xonotic
+
+## Overview
+
+This will install Xonotic on a linux box. Tested with Ubuntu 15.10.
---
+++ manifests/init.pp
@@ -0,0 +1,15 @@
+# == Class: xonotic
+#
+# Installs Xonotic
+#
+# === Authors
+#
+# Tyler Mauthe <me@tylermauthe.com>
+#
+# === Copyright
+#
+# Copyright 2015 Tyler Mauthe, unless otherwise noted.
+#
+class xonotic {
+
+}
